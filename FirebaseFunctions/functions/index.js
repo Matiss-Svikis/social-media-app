a2c301573085471b578dcdc6aa8f9142af147ab5
Matiss Svikis <matisssvikisss@gmail.com>
2021-01-12 08:09:06 +0200

    changed to express routing

--- FirebaseFunctions/functions/index.js
+++ FirebaseFunctions/functions/index.js
@@ -3,30 +3,25 @@ const admin = require('firebase-admin');
 
 admin.initializeApp();
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-exports.helloWorld = functions.https.onRequest((request, response) => {
-  functions.logger.info("Hello logs!", {structuredData: true});
-  response.send("Hello from world!");
-});
+const express = require('express');
+const app = express();
 
-exports.getScreams = functions.https.onRequest((request, response) =>{
+app.get('/scream', (request, response) =>{
     admin.firestore().collection('screams').get()
     .then(data =>{
         let screams = [];
         data.forEach(doc =>{
-            this.getScreams.push(doc.data());
+            screams.push({
+                screamId: doc.id,
+                ...doc.data()
+            });
         });
         return response.json(screams);
     })
     .catch((error)=>{console.error(error)})
 });
 
-exports.createScream = functions.https.onRequest((request, response) =>{
-    if(request.method!=="POST"){
-        return response.status(400).json({error: "Method not allowed"});
-    }
+app.post('/scream', (request, response)=>{
     const newScream = {
         body:request.body.body,
         userHandle:request.body.userHandle,
@@ -42,5 +37,7 @@ exports.createScream = functions.https.onRequest((request, response) =>{
         .catch(error=>{
             response.status(500).json({error: 'something went wrong'})
             console.error(error);
-        })
+        });
 });
+
+exports.api = functions.https.onRequest(app);
