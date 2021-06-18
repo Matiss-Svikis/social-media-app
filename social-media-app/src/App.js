d677ea1a5819afc7269899c3402e5db04355ab30
Matiss-Svikis <matisssvikisss@gmail.com>
2021-06-19 00:02:54 +0300

    added blank pages & navbar

--- social-media-app/src/App.js
+++ social-media-app/src/App.js
@@ -1,9 +1,27 @@
 import './App.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+//pages
+import home from "./pages/home"
+import login from "./pages/login"
+import signup from "./pages/signup"
+
+//componente
+import Navbar from './components/Navbar';
+
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <Router>
+        <Navbar />
+        <div className="container">
+          <Switch>
+            <Route exact path="/" component={home} />
+            <Route exact path="/login" component={login} />
+            <Route exact path="/signup" component={signup} />
+          </Switch>
+        </div>
+      </Router>
     </div>
   );
 }
