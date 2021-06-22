252e089b99dee827b2576d489b2c43f56698f7fd
Matiss-Svikis <matisssvikisss@gmail.com>
2021-06-22 07:17:01 +0300

    modified authenticated state

--- social-media-app/src/App.js
+++ social-media-app/src/App.js
@@ -17,20 +17,26 @@ import AuthRoute from './util/AuthRoute';
 import jwtDecode from 'jwt-decode';
 import { Component } from 'react';
 import { Redirect } from "react-router-dom";
+
+import { SET_AUTHENTICATED } from './redux/types';
+import { logoutUser, getUserData } from './redux/actions/userActions';
+import axios from 'axios';
+
 const theme = createMuiTheme(themeFile);
-let authenticated;
 const token = localStorage.FBIdToken;
 if (token) {
   const decodedToken = jwtDecode(token);
 
   if (decodedToken.exp * 1000 < Date.now()) {
     if (!window.location.href.includes('/login')) {
+      store.dispatch(logoutUser())
       window.location.href = '/login';
-      authenticated = false;
     }
   }
   else {
-    authenticated = true;
+    store.dispatch({ type: SET_AUTHENTICATED });
+    axios.defaults.headers.common['Authorization'] = token;
+    store.dispatch(getUserData());
   }
 }
 
@@ -44,8 +50,8 @@ class App extends Component {
             <div className="container">
               <Switch>
                 <Route exact path="/" component={home} />
-                <AuthRoute exact path="/login" component={login} authenticated={authenticated} />
-                <AuthRoute exact path="/signup" component={signup} authenticated={authenticated} />
+                <AuthRoute exact path="/login" component={login} />
+                <AuthRoute exact path="/signup" component={signup} />
               </Switch>
             </div>
           </Router>
