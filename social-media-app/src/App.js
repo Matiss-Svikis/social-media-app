4f226ed1470f40d1118603ef4403425bcae55349
Matiss-Svikis <matisssvikisss@gmail.com>
2021-06-22 03:52:53 +0300

    fixed login page looping bug

--- social-media-app/src/App.js
+++ social-media-app/src/App.js
@@ -2,7 +2,8 @@ import './App.css';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import { ThemeProvider as MuiThemeProvider } from '@material-ui/core/styles';
 import createMuiTheme from '@material-ui/core/styles/createMuiTheme';
-
+import { Provider } from 'react-redux';
+import store from './redux/store';
 //pages
 import home from "./pages/home"
 import login from "./pages/login"
@@ -14,39 +15,44 @@ import themeFile from './util/theme';
 import AuthRoute from './util/AuthRoute';
 
 import jwtDecode from 'jwt-decode';
-
+import { Component } from 'react';
+import { Redirect } from "react-router-dom";
 const theme = createMuiTheme(themeFile);
 let authenticated;
 const token = localStorage.FBIdToken;
 if (token) {
   const decodedToken = jwtDecode(token);
-  console.log(decodedToken);
+
   if (decodedToken.exp * 1000 < Date.now()) {
-    window.location.href = '/login';
-    authenticated = false;
+    if (!window.location.href.includes('/login')) {
+      window.location.href = '/login';
+      authenticated = false;
+    }
   }
   else {
     authenticated = true;
   }
 }
 
-function App() {
-  return (
-    <MuiThemeProvider theme={theme}>
-      <div className="App">
-        <Router>
-          <Navbar />
-          <div className="container">
-            <Switch>
-              <Route exact path="/" component={home} />
-              <AuthRoute exact path="/login" component={login} authenticated={authenticated} />
-              <AuthRoute exact path="/signup" component={signup} authenticated={authenticated} />
-            </Switch>
-          </div>
-        </Router>
-      </div>
-    </MuiThemeProvider>
-  );
+class App extends Component {
+  render() {
+    return (
+      <MuiThemeProvider theme={theme}>
+        <Provider store={store}>
+          <Router>
+            <Navbar />
+            <div className="container">
+              <Switch>
+                <Route exact path="/" component={home} />
+                <AuthRoute exact path="/login" component={login} authenticated={authenticated} />
+                <AuthRoute exact path="/signup" component={signup} authenticated={authenticated} />
+              </Switch>
+            </div>
+          </Router>
+        </Provider>
+      </MuiThemeProvider>
+    );
+  }
 }
 
 export default App;
