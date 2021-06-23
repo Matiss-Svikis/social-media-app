ac8dd801911be43624aa674c76f7a2f5258a47dd
Matiss-Svikis <matisssvikisss@gmail.com>
2021-06-23 07:16:44 +0300

    added conditional navbar buttons

--- social-media-app/src/components/Navbar.js
+++ social-media-app/src/components/Navbar.js
@@ -1,22 +1,61 @@
-import React, { Component } from 'react'
-import { Link } from 'react-router-dom';
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
 //MUI imports
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import Button from "@material-ui/core/Button";
-
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
+import { Tooltip } from "@material-ui/core";
+import MyButton from "../util/MyButton";
+import AddIcon from "@material-ui/icons/Add";
+import HomeIcon from "@material-ui/icons/Home";
+import Notifications from "@material-ui/icons/Notifications";
 export class Navbar extends Component {
-    render() {
-        return (
-            <AppBar>
-                <Toolbar className="nav-container">
-                    <Button color="inherit" component={Link} to="/login">Login</Button>
-                    <Button color="inherit" component={Link} to="/">Home</Button>
-                    <Button color="inherit" component={Link} to="/signup">Signup</Button>
-                </Toolbar>
-            </AppBar>
-        )
-    }
+  render() {
+    const { authenticated } = this.props;
+    return (
+      <AppBar>
+        <Toolbar className="nav-container">
+          {authenticated ? (
+            <>
+              <MyButton tip="Post a Scream">
+                <AddIcon />
+              </MyButton>
+              <Link to="/">
+                <MyButton tip="Home">
+                  <HomeIcon />
+                </MyButton>
+              </Link>
+              <MyButton tip="Notifications">
+                <Notifications />
+              </MyButton>
+            </>
+          ) : (
+            <>
+              <Button color="inherit" component={Link} to="/login">
+                Login
+              </Button>
+              <Button color="inherit" component={Link} to="/">
+                Home
+              </Button>
+              <Button color="inherit" component={Link} to="/signup">
+                Signup
+              </Button>
+            </>
+          )}
+        </Toolbar>
+      </AppBar>
+    );
+  }
 }
 
-export default Navbar
+Navbar.propTypes = {
+  authenticated: PropTypes.bool.isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  authenticated: state.user.authenticated,
+});
+
+export default connect(mapStateToProps)(Navbar);
