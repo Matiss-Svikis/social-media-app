e6fe4103641227b6ecaf8a79c20e772781dd1bb4
Matiss-Svikis <matisssvikisss@gmail.com>
2021-06-23 07:16:54 +0300

    formatting

--- social-media-app/src/pages/home.js
+++ social-media-app/src/pages/home.js
@@ -1,40 +1,42 @@
-import React, { Component } from 'react'
-import Grid from '@material-ui/core/Grid'
+import React, { Component } from "react";
+import Grid from "@material-ui/core/Grid";
 import axios from "axios";
 import Scream from "../components/Scream.js";
-import Profile from '../components/Profile.js'
+import Profile from "../components/Profile.js";
 export class home extends Component {
-    state = {
-        screams: null
-    }
-    componentDidMount() {
-        axios.get("/screams")
-            .then(res => {
-                this.setState({
-                    screams: res.data
-                })
-            })
-            .catch(err => console.error(err));
-    }
+  state = {
+    screams: null,
+  };
+  componentDidMount() {
+    axios
+      .get("/screams")
+      .then((res) => {
+        this.setState({
+          screams: res.data,
+        });
+      })
+      .catch((err) => console.error(err));
+  }
 
-
-    render() {
-        let recentScreamsMarkup = this.state.screams ? (
-            this.state.screams.map(scream => {
-                return (<Scream key={scream.screamId} scream={scream}></Scream>)
-            })
-        ) : (<p>loading...</p>);
-        return (
-            <Grid container spacing={10}>
-                <Grid item sm={8} xs={12}>
-                    {recentScreamsMarkup}
-                </Grid>
-                <Grid item sm={4} xs={12}>
-                    <Profile />
-                </Grid>
-            </Grid>
-        )
-    }
+  render() {
+    let recentScreamsMarkup = this.state.screams ? (
+      this.state.screams.map((scream) => {
+        return <Scream key={scream.screamId} scream={scream}></Scream>;
+      })
+    ) : (
+      <p>loading...</p>
+    );
+    return (
+      <Grid container spacing={10}>
+        <Grid item sm={8} xs={12}>
+          {recentScreamsMarkup}
+        </Grid>
+        <Grid item sm={4} xs={12}>
+          <Profile />
+        </Grid>
+      </Grid>
+    );
+  }
 }
 
-export default home
+export default home;
