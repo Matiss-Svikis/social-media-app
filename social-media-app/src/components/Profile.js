d35e54de7a38e7f473ca11d526eb86c1dacaa417
Matiss-Svikis <matisssvikisss@gmail.com>
2021-06-23 06:48:44 +0300

    added edit button

--- social-media-app/src/components/Profile.js
+++ social-media-app/src/components/Profile.js
@@ -15,6 +15,7 @@ import IconButton from '@material-ui/core/IconButton'
 import EditIcon from '@material-ui/icons/Edit';
 import { Tooltip } from '@material-ui/core';
 import KeyboardReturn from '@material-ui/icons/KeyboardReturn';
+import EditDetails from './EditDetails'
 const styles = (theme) => ({
     paper: {
         padding: 20
@@ -130,6 +131,7 @@ class Profile extends Component {
                             <KeyboardReturn color='primary' />
                         </IconButton>
                     </Tooltip>
+                    <EditDetails />
                 </div>
             </Paper>
         ) : (
